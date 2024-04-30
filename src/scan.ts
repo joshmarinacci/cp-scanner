7fab9a4e760eaec0ff4b293f303b6fbbf1f6d2b3
Josh Marinacci <276938+joshmarinacci@users.noreply.github.com>
2024-04-30 16:03:44 -0700

    parse out mpy version numbers get latest CP version number from the web

--- src/scan.ts
+++ src/scan.ts
@@ -20,11 +20,14 @@ console.log("files are",files)
 
 const boot_text = await fs.promises.readFile(path.join(cp_drive,'boot_out.txt'))
 const boot_info = boot_text.toString().split('\n')
+
+let board_id = null
 for(let line of boot_info) {
     console.log("boot line: ", line)
     let res = line.match(/^Board ID:(?<board_id>.*)/)
     if(res && res.groups) {
         console.log("found the board", res.groups['board_id'])
+        board_id = res.groups['board_id']
     }
     let UID_regex = line.match(/^UID:(?<uid>.*)/)
     if(UID_regex && UID_regex.groups) {
@@ -37,3 +40,37 @@ for(let line of boot_info) {
 }
 
 
+
+let libs = await fs.promises.readdir(path.join(cp_drive,'lib'))
+// console.log("libs",libs)
+for(let lib_name of libs) {
+    // console.log("parsing",lib_name)
+    let lib_path =path.join(cp_drive,'lib',lib_name)
+    let stats = await fs.promises.stat(lib_path)
+    if(stats.isFile()) {
+        console.log("library ",lib_name);
+        let lib_raw = await fs.promises.readFile(lib_path)
+        let mpy_version = String.fromCodePoint(lib_raw[0]);
+        let sub_version = lib_raw[1]
+        // console.log(`mpy_version ${mpy_version} . ${sub_version}`)
+        if(mpy_version === 'C' && sub_version === 6) {
+            // console.log("two byte mpy version 6")
+            let compatibility = ["9.0.0-alpha.1","none"]
+            let lib_str = lib_raw.toString("utf-8")
+            let match = lib_str.match(/(?<lib_version>[\d]+\.[\d]+\.[\d]+)\x00/)
+            if(match && match.groups) {
+                console.log("library version number is", match.groups['lib_version']);
+            }
+        }
+        // console.log("first bytes are",String.fromCodePoint(lib_raw[0]), lib_raw[1])
+    }
+}
+
+
+const release_url = 'https://github.com/adafruit/circuitpython/releases/latest'
+console.log("looking for the latest release")
+let resp = await fetch(release_url)
+let latest_cp_release = resp.url.split("/").at(-1)
+console.log("the response is",latest_cp_release)
+
+console.log(`a new version of CP for your board can be found at https://circuitpython.org/board/${board_id}`)
